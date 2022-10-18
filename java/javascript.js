55e7055c0b0c25ec2356e0a7a65caf0fefdfadb2
bvv94 <bvdevasconcelos@gmail.com>
2022-10-18 11:17:38 -0400

    carta com imagem trocando pro gif

---
+++ java/javascript.js
@@ -0,0 +1,9 @@
+function teste(){
+    alert ('oi');
+}
+function turn (card){
+    
+    card.innerHTML = `<img src="./files/bobrossparrot.gif">'`;
+    const aux = card.innerHTML;
+    alert(aux);
+}
