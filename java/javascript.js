d8c323bfd82801ee8913835f71fda0594590584a
bvv94 <bvdevasconcelos@gmail.com>
2022-10-19 15:24:13 -0400

    com versionamento

--- java/javascript.js
+++ java/javascript.js
@@ -1,20 +1,20 @@
 
-const jogo = Number(prompt('Com quantas cartas deseja jogar?'))
-let cartas = Number(jogo);
+let jogo = Number(prompt('Com quantas cartas deseja jogar?'))
+
 do {
-    if ((cartas%2!==0)||(cartas < 4) || (cartas > 14)) {
-        cartas = prompt('Informe um valor par (entre 4-14)!');
+    if ((jogo % 2 !== 0) || (jogo < 4) || (jogo > 14)) {
+        jogo = prompt('Informe um valor par (entre 4-14)!');
     }
-} while ((cartas%2!==0)||(cartas<4)||(cartas>14));
+} while ((jogo % 2 !== 0) || (jogo < 4) || (jogo > 14));
 
 const cards = []; //array das cartas
 
-function tabuleito() {
-    i = 0;
-    while (i <= cartas) {
-        cards.push();
-    }
+i = 0;
+while (i <= jogo) {
+    cards.push(document.querySelector('card'));
+    console.log(card[i]);
 }
+
 
 
 function game() {
@@ -24,10 +24,10 @@ function game() {
 function turn(card) {
     card.innerHTML = `<img src="./files/bobrossparrot.gif">'`;
     const aux = card.innerHTML; //virou pro gif
-    const intervalo = window.setTimeout(turn, 100);
+    const intervalo = window.setTimeout(self, 100);
     // function click();
     //alert(aux);
-    // setTimeout(30);
+     setTimeout(30);
     // function turnback(this);
 }
 
